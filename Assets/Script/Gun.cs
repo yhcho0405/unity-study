6d5f743470f64be6a0c6b3c605317fd5179ab275
yhcho0405 <yhcho0405@kakao.com>
2020-08-08 03:57:36 +0900

    added Enemy

--- Assets/Script/Gun.cs
+++ Assets/Script/Gun.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Gun : MonoBehaviour {
+    public Transform muzzle;
+    public Projectile projectile;
+    public float msBetweenShots = 100;
+    public float muzzleVelocity = 35;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    float nextShotTime;
+
+    public void Shoot() {
+        if (Time.time > nextShotTime) {
+            nextShotTime = Time.time + msBetweenShots / 1000;
+            Projectile newProjectile = Instantiate(projectile, muzzle.position, muzzle.rotation) as Projectile;
+            newProjectile.SetSpeed(muzzleVelocity);
+        }
     }
 }
--- Assets/Script/GunController.cs
+++ Assets/Script/GunController.cs
@@ -20,4 +20,10 @@ public class GunController : MonoBehaviour {
         equippedGun = Instantiate(gunToEquip, weaponHold.position, weaponHold.rotation) as Gun;
         equippedGun.transform.parent = weaponHold;
     }
+
+    public void Shoot() {
+        if (equippedGun != null) {
+            equippedGun.Shoot();
+        }
+    }
 }
